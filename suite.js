315f45c7fc38f2954cfb215a7416527e57ba89e3
Sam Thorogood <sam.thorogood@gmail.com>
2015-06-07 10:07:37 +1000

    use better Mocha TDD (setup/teardown), adds recentering test

--- suite.js
+++ suite.js
@@ -15,56 +15,99 @@
  */
 
 
-/**
- * Builds a test suite that has a default dialog.
- *
- * @param {string} name
- * @param {!Function(!HTMLDialogElement)} fn to run
- */
-function dialogTest(name, fn) {
-  test(name, function() {
-    var dialog = document.createElement('dialog');
+void function() {
+  var dialog;  // global dialog for all tests
+
+  /**
+   * Asserts that the displayed dialog is in the center of the screen.
+   *
+   * @param {HTMLDialogElement?} opt_dialog to check, or test default
+   */
+  function checkDialogCenter(opt_dialog) {
+    var d = opt_dialog || dialog;
+    var expectedTop = (window.innerHeight - d.offsetHeight) / 2;
+    var expectedLeft = (window.innerWidth - d.offsetWidth) / 2;
+    var rect = d.getBoundingClientRect();
+    assert.closeTo(rect.top, expectedTop, 1);
+    assert.closeTo(rect.left, expectedLeft, 1);
+  }
+
+  setup(function() {
+    dialog = document.createElement('dialog');
     dialog.innerText = 'Hello';
     document.body.appendChild(dialog);
     dialogPolyfill.registerDialog(dialog);
+  });
 
+  teardown(function() {
     try {
-      fn(dialog);
-    } finally {
-      if (dialog.parentElement) {
-        dialog.parentElement.removeChild(dialog);
-      }
+      dialog.close();
+    } catch (e) {}
+    if (dialog && dialog.parentElement) {
+      dialog.parentElement.removeChild(dialog);
     }
   });
-}
 
-dialogTest('basic dialog operation', function(dialog) {
-  assert(!dialog.hasAttribute('open'));
-  dialog.show();
-  assert(dialog.hasAttribute('open'));
-
-  var returnValue = 1234;
-  dialog.close(returnValue);
-  assert(!dialog.hasAttribute('open'));
-  assert.equal(dialog.returnValue, returnValue);
-});
-
-suite('centering', function() {
-
-  dialogTest('default centering', function(dialog) {
+  test('basic', function() {
+    assert.isFalse(dialog.hasAttribute('open'));
     dialog.show();
+    assert.isTrue(dialog.hasAttribute('open'));
 
-    var expectedTop = (window.innerHeight - dialog.offsetHeight) / 2;
-    var expectedLeft = (window.innerWidth - dialog.offsetWidth) / 2;
-    var rect = dialog.getBoundingClientRect();
-
-    assert(Math.abs(rect.top - expectedTop) < 2, 'expected almost equal top');
-    assert(Math.abs(rect.left - expectedLeft) < 2,
-        'expected almost equal left');
+    var returnValue = 1234;
+    dialog.close(returnValue);
+    assert.isFalse(dialog.hasAttribute('open'));
+    assert.equal(dialog.returnValue, returnValue);
   });
 
-});
+  suite('position', function() {
+    test('default centering', function() {
+      dialog.show();
+      checkDialogCenter();
+    });
+    test('respect static position', function() {
+      dialog.style.top = '10px';
+      dialog.show();
 
-suite('backdrop', function() {
+      var rect = dialog.getBoundingClientRect();
+      assert.equal(rect.top, 10);
+    });
+    test('recentering', function() {
+      var pX = document.body.scrollLeft;
+      var pY = document.body.scrollTop;
+      var big = document.createElement('div');
+      big.style.height = '200vh';  // 2x view height
+      document.body.appendChild(big);
 
-});
+      try {
+        var scrollValue = 200;  // don't use incredibly large values
+        dialog.show();
+        dialog.close();
+
+        window.scrollTo(0, scrollValue);
+        dialog.show();
+        checkDialogCenter();  // must be centered, even after scroll
+        var rectAtScroll = dialog.getBoundingClientRect();
+
+        // after scroll, we aren't recentered, check offset
+        window.scrollTo(0, 0);
+        var rect = dialog.getBoundingClientRect();
+        assert.closeTo(rectAtScroll.top + scrollValue, rect.top, 1);
+      } finally {
+        document.body.removeChild(big);
+        window.scrollTo(pX, pY);
+      }
+    });
+  });
+
+  suite('backdrop', function() {
+    test('backdrop div on modal', function() {
+      dialog.show();
+      assert.isNull(document.querySelector('.backdrop'));
+      dialog.close();
+
+      dialog.showModal();
+      assert.isNotNull(document.querySelector('.backdrop'));
+    });
+  });
+
+}();
