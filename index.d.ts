167567548c096ef4f3df93f28d62485286b4340f
Sam Thorogood <sam.thorogood@gmail.com>
2020-07-27 10:08:34 +1000

    add types

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+
+interface DialogPolyfillType {
+  registerDialog(dialog: HTMLDialogElement): void;
+  forceRegisterDialog(dialog: HTMLDialogElement): void;
+}
+
+/**
+ * If used as CJS, then "dialogPolyfill" is added to the global scope. Just assert it exists.
+ */
+declare global {
+  const dialogPolyfill: DialogPolyfillType;
+}
+
+/**
+ * If used as ESM, then we export the type of "DialogPolyfillType" as the default type.
+ */
+export default DialogPolyfill;
